31ce4c31d93ddb7a0314cdf4672bb20a3a0d91a5
lumen <lumen.hx@live.com>
2020-04-13 22:37:40 +0800

    拦截器完成

--- SpringBootProjectLean/src/main/java/com/lean/lumen/controller/indexContorller.java
+++ SpringBootProjectLean/src/main/java/com/lean/lumen/controller/indexContorller.java
@@ -2,9 +2,7 @@ package com.lean.lumen.controller;
 
 import com.lean.lumen.dto.PaginationDTO;
 import com.lean.lumen.dto.QuestionDTO;
-import com.lean.lumen.mapper.QuestionMapper;
 import com.lean.lumen.mapper.UserMapper;
-import com.lean.lumen.model.User;
 import com.lean.lumen.service.QuestionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,9 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class indexContorller {
@@ -27,25 +22,10 @@ public class indexContorller {
 
 
     @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model,
-                        @RequestParam(name = "page",defaultValue = "1") Integer page,
-                        @RequestParam(name = "size",defaultValue = "5") Integer size){
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
         // 打开首页时去查询cookie, 如果查询到cookies的话, 就存入session并自动登录
-        Cookie[] cookies = request.getCookies();
-        if (cookies == null){
-            return "index";
-        }
-        for (Cookie cookie : cookies) {
-            if("token".equals(cookie.getName())){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if (user != null){
-                    request.getSession().setAttribute("user", user);
-                }
-                break;
-            }
-        }
 
         PaginationDTO<QuestionDTO> paginationQuestion = questionService.question_list(page, size);
         model.addAttribute("questions", paginationQuestion);
---
+++ SpringBootProjectLean/src/main/java/com/lean/lumen/interceptor/SessionInterceptor.java
@@ -0,0 +1,48 @@
+package com.lean.lumen.interceptor;
+
+import com.lean.lumen.mapper.UserMapper;
+import com.lean.lumen.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class SessionInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length != 0) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals("token")) {
+                    String token = c.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+        // true 就会继续 执行
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
