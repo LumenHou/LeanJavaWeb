86f1936b7f99b746ea14933ab690a69f160ebfa5
lumen <lumen.hx@live.com>
2020-06-21 15:02:10 +0800

    新增securityLean模块

---
+++ securityLean/src/test/java/com/lean/lumen/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lean.lumen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
